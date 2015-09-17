cd82476248b3cbcbe84dee91b04ab23e9b53a675
eyolas <dtouzet@gmail.com>
2015-09-17 14:21:53 +0200

    correction bug async

--- dist/typeahead.jquery.js
+++ dist/typeahead.jquery.js
@@ -807,8 +807,8 @@
                     suggestions = suggestions || [];
                     if (!canceled && rendered < that.limit) {
                         that.cancel = $.noop;
-                        rendered += suggestions.length;
                         that._append(query, suggestions.slice(0, that.limit - rendered));
+                        rendered += that.limit - rendered;
                         that.async && that.trigger("asyncReceived", query);
                     }
                 }
